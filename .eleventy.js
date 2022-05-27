935e7491f327374a02831c2c215032b689f7682b
Robert C. Bowman III <bowman@opuscule.com>
2022-05-27 11:25:33 -0400

    passthru

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/images');
 	eleventyConfig.addPassthroughCopy('./src/styles');
 	eleventyConfig.addPassthroughCopy('./src/scripts');
+	eleventyConfig.addPassthroughCopy('./src/admin');
 	
 	return {
 		dir: {
